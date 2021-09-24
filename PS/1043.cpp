fd8e80738ff1a45a5b98718bea4a7523bfe29da9
SeoyunKIM <ab9110033@gmail.com>
2021-09-24 20:09:29 +0900

    solved

--- PS/1043.cpp
+++ PS/1043.cpp
@@ -6,8 +6,23 @@
 
 using namespace std;
 
-int truth[55], party[55], partyv[55];
-vector<int> people[55];
+int truth[55];
+int check_peo[55], check_par[55];
+vector<int> people[55], party[55];
+
+void ww(int x){ //x는 사람
+  if(check_peo[x])
+    return;
+  check_peo[x]=1;
+  for(int i: people[x]){
+    if(check_par[i])
+      continue;
+    check_par[i]=1;
+    for(int j: party[i]){
+      ww(j);
+    }
+  }
+}
 
 int n, m, o;
 int main(){
@@ -25,8 +40,20 @@ int main(){
     cin>>x;
     for(j=0; j<x; j++){
       cin>>k;
-      people
+      people[k].push_back(i); //k번 사람이 참여한 파티(i)
+      party[i].push_back(k); //i번 파티에 참여한 사람(k)
     }
   }
 
+  for(i=1; i<=n; i++){
+    if(truth[i]){
+      ww(i);
+    }
+  }
+
+  int count=0;
+  for(i=0; i<m; i++){
+    count+=!check_par[i];
+  }
+  cout<<count;
 }
