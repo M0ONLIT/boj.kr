41ab3b6b58d4504cf6707caf5eb64c9dd6b13c7f
SeoyunKIM <ab9110033@gmail.com>
2021-09-24 17:49:02 +0900

    solving

---
+++ PS/1043.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm>
+#include<cmath>
+#include<cstdio>
+#include<vector>
+
+using namespace std;
+
+int truth[55], party[55], partyv[55];
+vector<int> people[55];
+
+int n, m, o;
+int main(){
+  ios_base::sync_with_stdio(NULL);
+  cin.tie(0); cout.tie(0);
+
+  int i,j,k,x;
+  cin>>n>>m>>o;
+  for(i=0; i<o; i++){
+    cin>>x;
+    truth[x]=1;
+  }
+
+  for(i=0; i<m; i++){
+    cin>>x;
+    for(j=0; j<x; j++){
+      cin>>k;
+      people
+    }
+  }
+
+}
