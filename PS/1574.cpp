b98093aa7797f16755163c930970811e30c345d8
SeoyunKIM <ab9110033@gmail.com>
2021-09-24 16:05:47 +0900

    solved

--- PS/1574.cpp
+++ PS/1574.cpp
@@ -1,18 +1,21 @@
 #include<iostream>
 #include<algorithm>
 #include<cmath>
+#include<vector>
 #include<memory.h>
 
 typedef long long ll;
 using namespace std;
 
-int n, check[105], connect[105];
-int arr[105][105];
+int r, c, n, check[305], connect[305];
+int arr[305][305];
 
-vector<int> tree[105];
+vector<int> tree[305];
 
 int dfs(int x){
   for(int i: tree[x]){
+    if(check[i])
+      continue;
     check[i]=1;
     if(connect[i]==0 || dfs(connect[i])){
       connect[i]=x;
@@ -23,22 +26,21 @@ int dfs(int x){
 }
 
 int main(){
-  int i,j;
-  char c;
-  scanf("%d", &n);
-  for(i=1; i<=n; i++){
-    for(j=1; j<=n; j++){
-      scanf("%c", &c);
-      if(c=='.'){
-        arr[i][j]=1;
-        tree[i].push_back(j);
-      }
-    }scanf("%*c");
+  int i, j, p, q;
+  scanf("%d%d%d", &r, &c, &n);
+  for(i=0; i<n; i++){
+    scanf("%d%d", &p, &q);
+    arr[p][q]=1;
   }
 
+  for(i=1; i<=r; i++)
+    for(j=1; j<=c; j++)
+      if(!arr[i][j])
+        tree[i].push_back(j);
+
   int count=0;
-  for(i=1; i<=n; i++){
-    fill(check, check+101, 0);
+  for(i=1; i<=r; i++){
+    fill(check, check+301, 0);
     count+=dfs(i);
   }
   printf("%d", count);
