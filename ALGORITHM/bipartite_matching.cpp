9b9e27b0e8fc7328801bb1116c44d44827b4a3fb
SeoyunKIM <ab9110033@gmail.com>
2023-10-01 21:29:31 +0900

    23.10.01

---
+++ ALGORITHM/bipartite_matching.cpp
@@ -0,0 +1,47 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <algorithm> //fill(v.begin(), v.end(), 0);
+#include <vector>
+#include <memory.h>
+using namespace std;
+
+int n, m, ans;
+vector<int> arr[1005];
+int memory[1005];
+int match[1005];
+
+int dfs(int x){
+  for(int i: arr[x]){
+    if(memory[i])
+      continue;
+
+    memory[i]=1;
+    if(match[i]==0 || dfs(match[i])){
+      match[i]=x;
+      //cout<<x<<' '<<i<<endl;
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int p, q;
+    cin>>n>>m;
+    for(int i=1; i<=n; i++){
+        cin>>p;
+        for(int j=0; j<p; j++){
+            cin>>q;
+            arr[i].push_back(q);
+        }
+    }
+    for(int i=1; i<=n; i++){
+        fill(memory, memory+1005, 0);
+        ans+=dfs(i);
+    }
+    cout<<ans;
+    return 0;
+}
